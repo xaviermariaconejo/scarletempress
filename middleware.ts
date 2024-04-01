4588f9eb48ad3103711472894cce7b7ecf3ff21d
Xavier Conejo <xavier.maria.conejo@gmail.com>
2024-04-01 20:24:15 +0200

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -30,14 +30,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
+  matcher: ['/((?!api|static|.*\\..*|_next|favicon.ico).*)'],
 };
